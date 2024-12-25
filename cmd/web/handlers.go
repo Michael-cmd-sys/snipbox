0d938c49b701a2e9d952f7bd6ec439cf2558dff5
Michael Kofi Awuni <michaelawunikofi@gmail.com>
2024-12-25 09:54:31 +0000

    feat: add template files for home page

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,6 +3,8 @@ package main
 import (
   "fmt"
   "net/http"
+  "html/template"
+  "log"
   "strconv"
 )
 
@@ -12,11 +14,26 @@ func home(w http.ResponseWriter, r *http.Request){
     return;
   }
 
-  w.Write([]byte("Welcome to --snipbox--. The only snippet management solution for your content creation."));
+  files := []string{
+    "./ui/html/base.tmpl.html",
+    "./ui/html/pages/home.tmpl.html",
+  }
+
+  ts, err := template.ParseFiles(files...);
+  if err != nil {
+    log.Println(err.Error());
+    http.Error(w, "Internal server error", http.StatusInternalServerError);
+  }
+
+  err = ts.ExecuteTemplate(w, "base", nil);
+  if err != nil {
+    log.Println(err.Error());
+    http.Error(w, "Internal Server Error", http.StatusInternalServerError);
+  }
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
-  id, err := strconv.Atoi(r.Query().Get("id"));
+  id, err := strconv.Atoi(r.URL.Query().Get("id"));
   if err != nil || id < 1 {
     http.NotFound(w, r);
     return;
