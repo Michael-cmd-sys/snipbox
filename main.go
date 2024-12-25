059a9a03234de7b6e1ec6a70aa46155cace0f10d
Michael Kofi Awuni <michaelawunikofi@gmail.com>
2024-12-25 00:21:16 +0000

    feat: add route handlers(mutexes)

--- main.go
+++ main.go
@@ -1,9 +1,47 @@
 package main
 
 import (
+  "net/http"
+  "log"
+  "strconv"
   "fmt"
 )
 
+func home(w http.ResponseWriter, r *http.Request){
+  if r.URL.Path != "/" {
+    http.NotFound(w, r);
+    return;
+  }
+
+  w.Write([]byte("Hello from the home page..."));
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request){
+  id, err := strconv.Atoi(r.URL.Query().Get("id"));
+  if err != nil || id < 1 {
+    http.NotFound(w, r);
+    return;
+  }
+  fmt.Fprintf(w, "Display a specific snippet with ID %d...", id);
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+  if r.Method != "POST" {
+    w.Header().Set("Allow", http.MethodPost);
+    http.Error(w, "Mehtod not allowed...", http.StatusMethodNotAllowed);
+    return;
+  }
+
+  w.Write([]byte("Hello from the snippet create path"));
+}
+
 func main(){
-  fmt.Println("Hello, World!");
+  mux := http.NewServeMux();
+  mux.HandleFunc("/", home);
+  mux.HandleFunc("/snippet/view", snippetView);
+  mux.HandleFunc("/snippet/create", snippetCreate);
+
+  log.Println("Starting server on :5000");
+  err := http.ListenAndServe(":5000", mux);
+  log.Fatal(err);
 }
