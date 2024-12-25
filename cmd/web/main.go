e82c47d39c34283fbdc7c7a5baa3b599d069dbbe
Michael Kofi Awuni <michaelawunikofi@gmail.com>
2024-12-25 11:47:37 +0000

    refactor: change FIXED port to allow for dynamic host port value

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,10 +3,15 @@ package main
 import (
   "net/http"
   "log"
+  "flag"
 )
 
 func main(){
+  addr := flag.String("addr", ":5000", "HTTP Network Address");
+  flag.Parse();
+
   mux := http.NewServeMux();
+
   fileServer := http.FileServer(http.Dir("./ui/static"));
 
   mux.Handle("/static/", http.StripPrefix("/static", fileServer));
@@ -14,7 +19,7 @@ func main(){
   mux.HandleFunc("/snippet/view", snippetView);
   mux.HandleFunc("/snippet/create", snippetCreate);
 
-  log.Println("Starting server on :5000");
-  err := http.ListenAndServe(":5000", mux);
+  log.Printf("Starting server on %s", *addr);
+  err := http.ListenAndServe(*addr, mux);
   log.Fatal(err);
 }
