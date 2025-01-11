d717a2884eb7886a37260b7efb4e2852860e64d2
Michael Kofi Awuni <michaelawunikofi@gmail.com>
2025-01-11 18:20:05 +0000

    refactor: rewrite templates with template actions and functions to allow for flexibility and more robust readable code

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -22,14 +22,10 @@ func (app *application) home(w http.ResponseWriter, r *http.Request){
     return
   }
 
-  for _, snippet := range snippets {
-    fmt.Fprintf(w, "%+v\n", snippet)
-  }
-
   files := []string{
     "./ui/html/base.tmpl.html",
+    "./ui/html/partials/nav.tmpl.html",
     "./ui/html/pages/home.tmpl.html",
-    "ui/html/partials/nav.tmpl.html",
   }
 
   ts, err := template.ParseFiles(files...);
@@ -37,9 +33,13 @@ func (app *application) home(w http.ResponseWriter, r *http.Request){
     app.serverError(w, err);
   }
 
-  err = ts.ExecuteTemplate(w, "base", nil);
+  data := &templateData{
+    Snippets: snippets,
+  }
+
+  err = ts.ExecuteTemplate(w, "base", data)
   if err != nil {
-    app.serverError(w, err);
+    app.serverError(w, err)
   }
 }
 
