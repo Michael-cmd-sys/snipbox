2e185a6fb30241cbe5a2a59c8177060c5b200b6c
Michael Kofi Awuni <michaelawunikofi@gmail.com>
2025-01-13 23:06:31 +0000

    refactor: minor changes and bug fixes relating to template files

--- ui/static/js/main.js
+++ ui/static/js/main.js
@@ -1,6 +1,6 @@
 var navLinks = document.querySelectorAll("nav a");
-for (var i = 0; i < navLinks.length; i++) {
-	var link = navLinks[i]
+for (const i = 0; i < navLinks.length; i++) {
+	const link = navLinks[i]
 	if (link.getAttribute('href') == window.location.pathname) {
 		link.classList.add("live");
 		break;
