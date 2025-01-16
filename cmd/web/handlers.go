91523781849f9224463df77b3810c7d08db58d3c
Michael Kofi Awuni <michaelawunikofi@gmail.com>
2025-01-16 22:10:55 +0000

    refactor: add new package  to help with converting query urls to clean urls

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,12 +1,13 @@
-package main 
+package main
 
 import (
-  "errors"
-  "fmt"
-  "net/http"
-  "strconv"
+	"errors"
+	"fmt"
+	"net/http"
+	"strconv"
 
-  "github.com/Michael-cmd-sys/snipbox/internal/models"
+	"github.com/Michael-cmd-sys/snipbox/internal/models"
+	"github.com/julienschmidt/httprouter"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request){
@@ -28,7 +29,9 @@ func (app *application) home(w http.ResponseWriter, r *http.Request){
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-  id, err := strconv.Atoi(r.URL.Query().Get("id"));
+  params := httprouter.ParamsFromContext(r.Context())
+
+  id, err := strconv.Atoi(params.ByName("id"))
   if err != nil || id < 1 {
     app.notFound(w);
     return;
@@ -51,21 +54,19 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-  if r.Method != http.MethodPost {
-    w.Header().Set("Allow", http.MethodPost);
-    app.clientError(w, http.StatusMethodNotAllowed);
-    return;
-  }
+  w.Write([]byte("Display the form for creating a new snippet.."))
+}
 
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
   title := "0 Snail"
-  content := "0 Snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n-Kobayashi Issa"
+  content := "0 Snail\nClimg Mount Fuji,\nBut Slowly, Slowly!\n\n-Kobashi Issa"
   expires := 7
 
   id, err := app.snippets.Insert(title, content, expires)
   if err != nil {
     app.serverError(w, err)
-    return 
+    return
   }
-  
-  http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+
+  http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
