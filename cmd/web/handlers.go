5e609c193826d26937c289cb43e05ce13b9a91c8
Michael Kofi Awuni <michaelawunikofi@gmail.com>
2025-01-19 23:01:25 +0000

    feat: add form validation in handlers to ensure integrity and scurity of application

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+  "strings"
+  "unicode/utf8"
 
 	"github.com/Michael-cmd-sys/snipbox/internal/models"
 	"github.com/julienschmidt/httprouter"
@@ -59,11 +61,44 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-  title := "0 Snail"
-  content := "0 Snail\nClimg Mount Fuji,\nBut Slowly, Slowly!\n\n-Kobashi Issa"
-  expires := 7
+  err := r.ParseForm()
+  if err != nil {
+    app.clientError(w, http.StatusBadRequest)
+    return 
+  }
 
-  id, err := app.snippets.Insert(title, content, expires)
+  title := r.PostForm.Get("title")
+  content := r.PostForm.Get("content")
+
+  expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+  if err != nil {
+    app.clientError(w, http.StatusBadRequest)
+    return
+  }
+
+  fieldErrors := make(map[string]string)
+  if strings.TrimSpace(title) == ""{
+    fieldErrors["title"] = "This field cannot be blank"
+  } else if utf8.RuneCountInString(title) > 50 {
+    fieldErrors["title"] = "This field cannot be more than 50 characters long"
+  }
+
+  if strings.TrimSpace(content) == ""{
+    fieldErrors["content"] = "This field cannot be blank"
+  } else if utf8.RuneCountInString(content) > 500 {
+    fieldErrors["content"] = "This field cannot be more than 250 chracters long"
+  }
+
+  if expires != 1 && expires != 7 && expires != 365 {
+    fieldErrors["expires"] = "This field must equal 1, 7, or 365"
+  }
+
+  if len(fieldErrors) > 0 {
+    fmt.Fprint(w, fieldErrors)
+    return
+  }
+  
+  id , err := app.snippets.Insert(title, content, expires)
   if err != nil {
     app.serverError(w, err)
     return
