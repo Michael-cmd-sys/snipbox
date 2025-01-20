e48ac1d122de0ab873f13dde1ab3275e452c8fd9
Michael Kofi Awuni <michaelawunikofi@gmail.com>
2025-01-20 15:59:54 +0000

    fix: styling and scripts not being applied tnot being appliedd to application

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -21,7 +21,7 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 
 func secureHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-		w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'self'; fonts.googleapis.com; font.gstatic.com")
+		w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'self' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com;")
 		w.Header().Set("Referrer-Policy", "origin-when-cross-origin")
 		w.Header().Set("X-Content-Type-Options", "nosniff")
 		w.Header().Set("X-Frame-Options", "deny")
