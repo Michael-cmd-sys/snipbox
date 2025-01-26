af4d6fba85b13aa5715528ba9ad6eb8045e57e4c
Michael Kofi Awuni <michaelawunikofi@gmail.com>
2025-01-26 23:12:15 +0000

    refactor: handle nil passes to the go-playground form management library

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -6,8 +6,30 @@ import (
 	"net/http"
 	"runtime/debug"
 	"time"
+	"errors"
+
+	"github.com/go-playground/form/v4"
 )
 
+
+func (app *application) decodePostForm(r *http.Request, dst any) error {
+	err := r.ParseForm()
+	if err != nil {
+		return err
+	}
+
+	err = app.formDecoder.Decode(dst, r.PostForm)
+	if err != nil {
+		var invalidDecoderError *form.InvalidDecoderError
+
+		if errors.As(err, &invalidDecoderError) {
+			panic(err)
+		}
+
+		return err
+	}
+	return nil
+}
 func (app *application) serverError(w http.ResponseWriter, err error) {
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack());
 	app.errorLog.Output(2, trace);
