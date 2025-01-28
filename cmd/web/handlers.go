6838e6e1e7cb7d387edf68e5eda404d9e74bd5a1
Michael Kofi Awuni <michaelawunikofi@gmail.com>
2025-01-28 23:45:51 +0000

    feat: upgrade http to https connections with TLS self-signed certs

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -57,6 +57,7 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 
     return
   }
+
   data := app.newTemplateData(r)
   data.Snippet = snippet
 
@@ -105,5 +106,6 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
     return
   }
 
+  app.sessionManager.Put(r.Context(), "flash", "Snippet successfully created!")
   http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
