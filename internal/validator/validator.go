f4f6e72ddbf485aa6501750ef512680802dd6a31
Michael Kofi Awuni <michaelawunikofi@gmail.com>
2025-01-30 22:46:55 +0000

    fix error as a result of redeclaration of Validator struct

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -29,14 +29,6 @@ func Matches(value string, rx *regexp.Regexp) bool {
 	return rx.MatchString(value)
 }
 
-type Validator struct {
-	FieldErrors map[string]string
-}
-
-func (v *Validator) Valid() bool {
-	return len(v.FieldErrors) == 0
-}
-
 func (v *Validator) AddFieldError(key, message string) {
 	if v.FieldErrors == nil {
 		v.FieldErrors = make(map[string]string)
